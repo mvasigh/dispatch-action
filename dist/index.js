6c2e84975d251e8f26673e0c0416f7eca4df47f7
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2020-03-11 07:15:40 -0500

    add build commit script

--- dist/index.js
+++ dist/index.js
@@ -534,8 +534,44 @@ module.exports = /******/ (function(modules, runtime) {
       const core = __webpack_require__(470);
       const github = __webpack_require__(469);
 
+      function getDispatchDest({ context, repo: _repo, owner: _owner }) {
+        let repo, owner;
+
+        if (_repo === '@') {
+          repo = context.repository.name;
+          owner = context.repository.owner.login;
+        } else if (_repo.includes('/')) {
+          [repo, owner] = _repo.split('/');
+        } else {
+          repo = _repo;
+          owner = _owner || context.repository.owner.login;
+        }
+
+        return {
+          repo,
+          owner
+        };
+      }
+
       (async function main() {
-        console.log('hello world!');
+        const { repo, owner } = getDispatchDest({
+          context: github.context.payload,
+          repo: core.getInput('repo'),
+          owner: core.getInput('owner')
+        });
+
+        const event_type = core.getInput('event_type');
+        const client_payload = core.getInput('client_payload');
+
+        const token = core.getInput('token');
+        const octokit = github.GitHub(token);
+
+        octokit.repos.createDispatchEvent({
+          owner,
+          repo,
+          event_type,
+          client_payload: client_payload ? JSON.stringify(client_payload) : null
+        });
       })();
 
       /***/
