f65cff4fce4ba4c62a00b4cc0260c52703b82c6c
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2020-03-13 09:16:16 -0500

    generate build

--- index.js
+++ index.js
@@ -30,19 +30,19 @@ function getDispatchDest({ context, repo: _repo, owner: _owner }) {
 
     const event_type = core.getInput('event_type');
     const client_payload = core.getInput('payload');
-    // console.log(client_payload);
+    console.log(client_payload);
 
     const token = core.getInput('token');
     const octokit = new github.GitHub(token);
 
     console.log('Dispatching the action with these parameters:');
-    // console.log({ owner, repo, event_type });
+    console.log({ owner, repo, event_type, client_payload });
 
     await octokit.repos.createDispatchEvent({
       owner,
       repo,
-      event_type
-      // client_payload: JSON.parse(client_payload) || {}
+      event_type,
+      client_payload: JSON.parse(client_payload) || {}
     });
 
     console.log('Dispatch event emitted successfully!');
